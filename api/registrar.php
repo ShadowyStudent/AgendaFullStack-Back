b0035b563a7f15c26662beb4e91961ffb9ed7ce2
Miguel Álvarez <alteregoreborn@gmail.com>
2025-11-27 22:33:12 -0600

    r

--- api/registrar.php
+++ api/registrar.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
+
 require_once __DIR__ . '/_cors.php';
 header('Content-Type: application/json; charset=utf-8');
 require_once __DIR__ . '/../config/db.php';
