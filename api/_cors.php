0c361996c2d8363693012f1e33db1f6a5c47845f
Miguel Álvarez <alteregoreborn@gmail.com>
2025-11-28 18:18:26 -0600

    _cors-php

--- api/_cors.php
+++ api/_cors.php
@@ -1,24 +1,12 @@
 <?php
-$whitelist = [
-    'https://shadowystudent.github.io',
-    'https://render.com',
-    'https://clever-cloud.com'
-];
-
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-if (in_array($origin, $whitelist, true)) {
-    header("Access-Control-Allow-Origin: $origin");
-} else {
-    header("Access-Control-Allow-Origin: https://shadowystudent.github.io");
-}
-
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Expose-Headers: Authorization, X-Total-Count');
-header('Access-Control-Max-Age: 600');
-header('Vary: Origin');
-header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept');
-header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Expose-Headers: Authorization, X-Total-Count");
+header("Access-Control-Max-Age: 600");
+header("Vary: Origin");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept");
+header("Content-Type: application/json; charset=utf-8");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
